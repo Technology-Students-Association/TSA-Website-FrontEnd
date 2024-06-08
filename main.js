476da70197e5731863737c3da30b78318bf27ee9
Lewis Machabe <lewismachabe@gmail.com>
2024-06-08 13:15:54 +0300

    humberger menu

--- main.js
+++ main.js
@@ -21,3 +21,24 @@ faqs.forEach((faq) => {
     }
   });
 });
+
+// Show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+// Close nav-menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+// Attach the closeNav function to the close button click event
+closeBtn.addEventListener("click", closeNav);
