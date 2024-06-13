644b9950faaad55eec0b44f32bdb8d0db8b37123
Joram <joram.nyamawimwanyika@kibo.school>
2024-06-13 11:42:48 +0300

    Final commit

--- contact.js
+++ contact.js
@@ -1,11 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.getElementById("navbar");
-
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 50) { // Change 50 to the number of pixels you want
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    });
-});
+// change navbar style on scroll
+window.addEventListener("scroll", () => {
+    document
+      .querySelector("nav")
+      .classList.toggle("window-scroll", window.scrollY > 0);
+  });
+  
+  
+  // Show/hide nav menu
+  const menu = document.querySelector(".nav__menu");
+  const menuBtn = document.querySelector("#open-menu-btn");
+  const closeBtn = document.querySelector("#close-menu-btn");
+  
+  menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+  });
+  
+  // Close nav-menu
+  const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+  };
+  
+  // Attach the closeNav function to the close button click event
+  closeBtn.addEventListener("click", closeNav);
+  
