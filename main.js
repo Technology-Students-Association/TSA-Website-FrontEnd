621294d8a17eb8b463ffa9bed88be93d40abe525
Lewis Machabe <lewismachabe@gmail.com>
2024-06-15 19:13:07 +0300

    styledabout us page

--- main.js
+++ main.js
@@ -42,3 +42,42 @@ const closeNav = () => {
 
 // Attach the closeNav function to the close button click event
 closeBtn.addEventListener("click", closeNav);
+
+// SLIDE SHOW PICTURES RIGHT
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Auto change slides every 5 seconds
+setInterval(() => {
+  plusSlides(1);
+}, 5000);
